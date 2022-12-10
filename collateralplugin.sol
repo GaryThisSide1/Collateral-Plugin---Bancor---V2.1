9d1f827e7d876f0174025d0708b60516e2e98b1c
GaryThisSide1 <119076200+GaryThisSide1@users.noreply.github.com>
2022-12-11 03:16:06 +0530

    Create collateralplugin.sol

---
+++ collateralplugin.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.6.0;
+
+// Bancor reserve protocol collateral plugin smart contract
+// This contract allows users to provide collateral for their reserve contributions on the Bancor network
+
+// Import necessary libraries
+import "https://github.com/bancorprotocol/contracts/BancorConverter.sol";
+import "https://github.com/bancorprotocol/contracts/BancorConverterRegistry.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/Escrow.sol";
+
+// Set contract as a BancorConverterRegistry plugin
+contract BancorCollateralPlugin is BancorConverterRegistryPlugin {
+    using SafeMath for uint256;
+    using Escrow for address;
+
+    // Define necessary variables
+    BancorConverterRegistry public registry;
+    mapping(address => Escrow) public escrowAccounts;
+    mapping(address => uint256) public reserveCollateral;
+    uint256 public collateralRequirement;
+    uint256 public collateralLiquidationThreshold;
+    address public collateralToken;
+
+    // Constructor function to initialize the contract
+    constructor(
+        address _registry,
+        uint256 _collateralRequirement,
+        uint256 _collateralLiquidationThreshold,
+        address _collateralToken
+    ) public {
+        // Set the BancorConverterRegistry contract address
+        registry = BancorConverterRegistry(_registry);
+
+        // Set the collateral requirement and liquidation threshold
+        collateralRequirement = _collateralRequirement;
+        collateralLiquidationThreshold = _collateralLiquidationThreshold;
+
+        // Set the collateral token
+        collateralToken = _collateralToken;
+    }
+
+    // Function to create a new reserve on the Bancor network
+    // This function requires the reserve contributor to provide collateral for their reserve
+    function createReserve(
+        address _reserve,
+        uint256 _reserveCollateral
+    ) external {
+        // Check that the reserve contributor has provided the required amount of collateral
+        require(_reserveCollateral >= collateralRequirement, "Insufficient collateral provided");
+
+        // Create an escrow account for the reserve contributor's collateral
+        escrowAccounts[_reserve] = new Escrow();
+
+        // Deposit the reserve contributor's collateral into the escrow account
+        escrowAccounts[_reserve].deposit(_reserveCollateral);
+
+        // Record the amount of collateral deposited by the reserve contributor
+        reserveCollateral[_reserve] = _reserveCollateral;
+
+        // Register the reserve with the BancorConverterRegistry contract
+        registry.register(_reserve);
+    }
+
+    // Function to modify an existing reserve on the Bancor network
+    // This function allows the reserve contributor to deposit additional collateral or withdraw excess collateral
+    function modifyReserve(
+        address _reserve,
+        uint256 _reserveCollateral
+    ) external {
+        // Check that the reserve contributor has provided the required amount of collateral
